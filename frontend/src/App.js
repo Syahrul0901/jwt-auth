0cc01bc4d7700e31123b980c9477de5d99a423c9
syahrul mubarok <mubaroksyahrul8132gmail.com>
2021-11-03 16:03:46 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
 
 function App() {
   return (
     <BrowserRouter>
-      <Login />
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/dashboard">
+          <Navbar />
+          <Dashboard />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
