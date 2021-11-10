b055840f48eb6cba252b4ee9f67aecccda8fff71
syahrul mubarok <mubaroksyahrul8132gmail.com>
2021-11-10 09:58:19 +0700

    updte

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -31,10 +31,30 @@ const Dashboard = () => {
     }
   };
 
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+        const response = await axios.get('http://localhost:5000/token');
+        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        setToken(response.data.accessToken);
+        const decoded = jwt_decode(response.data.accessToken);
+        setName(decoded.name);
+        setExpire(decoded.exp);
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
   const getUsers = async () => {
-    const response = await axios.get('http://localhost:5000/users', {
+    const response = await axiosJWT.get('http://localhost:5000/users', {
       headers: {
-        Authorization: 'Bearer ${token}',
+        Authorization: `Bearer ${token}`,
       },
     });
     console.log(response.data);
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,20 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Navbar = () => {
+  const history = useHistory();
+
+  const Logout = async () => {
+    try {
+      await axios.delete('http://localhost:5000/logout');
+      history.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <nav className="navbar is-light" role="navigation" aria-label="main navigation">
       <div className="container">
@@ -28,7 +40,9 @@ const Navbar = () => {
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
-                <button className="button is-light">Log out</button>
+                <button onClick={Logout} className="button is-light">
+                  Log out
+                </button>
               </div>
             </div>
           </div>
